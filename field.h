a644dcbcf86b60067f376bc82062a4d39ca1d0d6
Nick Higginbottom <nick_higginbottom@fastmail.fm>
2015-07-08 17:40:42 +0100

    lots

--- field.h
+++ field.h
@@ -1,11 +1,14 @@
       REAL   d(in,jn),  e(in,jn),  p(in,jn), v1(in,jn), v2(in,jn)
+#ifdef PYHYDRO		  
 	  REAL   xi(in,jn), e_old(in,jn), hc_comp(in,jn),hc_xray(in,jn)
+	  REAL   d_old(in,jn)
 	  REAL   hc_line(in,jn),hc_brem(in,jn),t_old(in,jn),adiab(in,jn)
 	  REAL   comp_pre(in,jn),brem_pre(in,jn)
 	  REAL   xray_pre(in,jn),line_pre(in,jn)
 	  REAL   divv1(in,jn),v1_pdv(in,jn),v2_pdv(in,jn)
 	  REAL   dt_csound(in,jn),dt_v1(in,jn),dt_v2(in,jn)
 	  REAL   dt_viscosity(in,jn),dt_alfen(in,jn)
+#endif
 #ifdef ROTATE
       REAL  v3(in,jn)
 #endif
@@ -21,6 +24,7 @@
 
       common /fieldr/  d ,  e,  p, v1, v2
 	  common /fieldr/  xi , e_old, t_old,adiab
+	  common /fieldr/  d_old
 	  common /fieldr/  hc_comp,hc_xray,hc_line,hc_brem
 	  common /fieldr/  comp_pre,xray_pre,line_pre,brem_pre
 
