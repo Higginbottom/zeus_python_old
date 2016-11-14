c6ba4fb1d9e243c0420fe6938a7c1a1565d4072b
Nick Higginbottom <nick_higginbottom@fastmail.fm>
2016-11-14 14:13:37 +0000

    some cahges

--- zeus_python.h
+++ zeus_python.h
@@ -14,8 +14,8 @@
 		
 	REAL, dimension(nxi) :: lu_xi
 	REAL, dimension(nt) ::lu_temp
-		REAL, dimension(nxi) :: log_lu_xi
-		REAL, dimension(nt) ::log_lu_temp
+	REAL, dimension(nxi) :: log_lu_xi
+	REAL, dimension(nt) ::log_lu_temp
 		
 	LOGICAL main_heatcool,indiv_heatcool,split_heatcool
 
@@ -23,4 +23,4 @@
 	common /lookup_data/ n_lu_temp,n_lu_xi,hc_lu,lu_xi,lu_temp,
      +  comp_h_lu,comp_c_lu,line_c_lu,xray_h_lu,brem_c_lu,
      +  main_heatcool,indiv_heatcool,log_lu_xi,log_lu_temp,
-     +  cool_lu,heat_lu
+     +  cool_lu,heat_lu,split_heatcool
